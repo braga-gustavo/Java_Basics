d7fc076137db375cfbf898dc0064ba59e20b2781
braga-gustavo <gustavo.b.moraes2@gmail.com>
2024-06-04 16:24:52 -0300

    using google books api

---
+++ basics/src/api/usage/principal/RequestGBooksApi.java
@@ -0,0 +1,27 @@
+package api.usage.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class RequestGBooksApi {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String apiAddress = "https://www.googleapis.com/books/v1/volumes?q=";
+        System.out.println("Book name: ");
+        String requestValue = scanner.nextLine();
+        final String apiKey = "AIzaSyAlRGtAck_JQjfm7tT8ovA0smpS-5L27-8";
+
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create(apiAddress.concat(requestValue.replaceAll(" ", "+")).concat("&key=").concat(apiKey))).build();
+        var reponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(reponse.body());
+    }
+
+}
--- basics/src/api/usage/principal/RequestOMDBApiApp.java
+++ basics/src/api/usage/principal/RequestOMDBApiApp.java
@@ -5,16 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
-public class RequestApiApp {
+public class RequestOMDBApiApp {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o nome do filme: ");
+        String requestValue = scanner.nextLine();
+        String apiAdress = "http://www.omdbapi.com/?t=";
+        String apiKey = "&apikey=2b301e2f";
+        
         HttpClient client = HttpClient.newHttpClient();
+        
+        //replaceAll replaces the first value by the second
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(
-                        "http://www.omdbapi.com/?t=The+lord+of+the+rings%3A+the+return+of+the+king&apikey=2b301e2f"))
-                .build();
+                .uri(URI.create(apiAdress.concat(requestValue.replaceAll(" ", "+")).concat(apiKey))).build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
